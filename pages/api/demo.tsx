73007fc14c2e10c5ae65622e7a968db14eca70bd
何凯 <F160980@efivestar.com>
2020-08-31 18:04:52 +0800

    blog init

---
+++ pages/api/demo.tsx
@@ -0,0 +1,11 @@
+import {NextApiHandler} from 'next';
+
+const Demo: NextApiHandler = (req, res) => {
+    // 其他的操作和 js 一样
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.write(JSON.stringify({name: '狗子'}));
+  res.end();
+};
+
+export default Demo;
---
+++ pages/posts/first-post.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const FirstPost: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      fss
+    </div>
+  )
+}
+export default FirstPost
